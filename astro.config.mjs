aa6adeb4610037241a037bb2206ca536c8fb7084
Bruno Andrade <brunoan99@gmail.com>
2025-04-05 18:05:19 -0300

    chore: update astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.brunoan.dev',
-  image: {
-    service: squooshImageService(),
-  },
-	integrations: [mdx(), sitemap()],
+  site: 'https://blog.brunoan.dev',
+  integrations: [mdx(), sitemap()],
 });
